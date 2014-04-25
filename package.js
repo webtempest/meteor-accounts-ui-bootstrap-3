92524f3d5aa471b6d01910ec7647553b938d36a6
Yasin Uslu <nepjua@gmail.com>
2014-04-25 22:28:20 +0300

    starting point for complete rewrite

--- package.js
+++ package.js
@@ -1,32 +1,36 @@
 Package.describe({
-  summary: "Bootstrap styled version of login widgets"
+  summary: "Bootstrap-3 styled accounts-ui"
 });
 
 Package.on_use(function (api) {
-  api.use(['session', 'handlebars', 'stylus', 'accounts-base', 'underscore', 'templating'], 'client');
+  api.use(['deps', 'jquery', 'service-configuration', 'accounts-base',
+           'underscore', 'templating', 'session', 'less'], 'client');
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+
+  // Allow us to call Accounts.oauth.serviceNames, if there are any OAuth
+  // services.
+  api.use('accounts-oauth', {weak: true});
+
+  // Allow us to directly test if accounts-password (which doesn't use
+  // Accounts.oauth.registerService) exists.
+  api.use('accounts-password', {weak: true});
+
+
+  api.use('just-i18n', 'client');
 
   api.add_files([
-    'accounts_ui.js',
-
-    'login_buttons_images.css',
-    'login_buttons_dropdown.styl',
-
-    'login_buttons.html',
-    'login_buttons_single.html',
-    'login_buttons_dropdown.html',
-    'login_buttons_dialogs.html',
-
-    'login_buttons_session.js',
-
-    'login_buttons.js',
-    'login_buttons_single.js',
-    'login_buttons_dropdown.js',
-    'login_buttons_dialogs.js',
-    'accounts_ui.styl'], 'client');
+    "lib/utils.js",
+    "lib/accounts-ui.js",
+    "lib/en.js",
+    'lib/login-buttons.html',
+    'lib/login-buttons.js',
+    'lib/form.js',
+    'lib/dropdown.less',
+  ], 'client');
 });
 
 Package.on_test(function (api) {
-  //api.use('meteor-accounts-ui-bootstrap');
-  //api.use('tinytest');
-  //api.add_files('accounts_ui_tests.js', 'client');
+  api.use('accounts-ui-bootstrap-3');
+  api.use('tinytest');
 });
