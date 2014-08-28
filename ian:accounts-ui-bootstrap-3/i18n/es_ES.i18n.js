fed8a4e13d43bfc54703df60c55606b5ff397e24
Ian Martorell <ianmartorell@gmail.com>
2014-08-28 18:13:38 +0200

    Added Spanish

---
+++ ian:accounts-ui-bootstrap-3/i18n/es_ES.i18n.js
@@ -0,0 +1,70 @@
+i18n.map('es_ES', {
+	_resetPasswordDialog: {
+		title: 'Restablece tu contraseña',
+		cancel: 'Cancelar',
+		submit: 'Guardar'
+	},
+	_enrollAccountDialog: {
+		title: 'Escribe una contraseña',
+		cancel: 'Cerrar',
+		submit: 'Guardar contraseña'
+	},
+	_justVerifiedEmailDialog: {
+		verified: 'Correo electrónico verificado',
+		dismiss: 'Cerrar'
+	},
+	_loginButtonsMessagesDialog: {
+		dismiss: 'Cerrar',
+	},
+	_loginButtonsLoggedInDropdownActions: {
+		password: 'Cambiar contraseña',
+		signOut: 'Cerrar sesión'
+	},
+	_loginButtonsLoggedOutDropdown: {
+		signIn: 'Iniciar sesión',
+		up: 'registrarse'
+	},
+	_loginButtonsLoggedOutPasswordServiceSeparator: {
+		or: 'o'
+	},
+	_loginButtonsLoggedOutPasswordService: {
+		create: 'Crear',
+		signIn: 'Iniciar sesión',
+		forgot: 'Ha olvidado su contraseña?',
+		createAcc: 'Registrarse'
+	},
+	_forgotPasswordForm: {
+		reset: 'Restablecer contraseña'
+	},
+	_loginButtonsBackToLoginLink: {
+		back: 'Cancelar'
+	},
+	_loginButtonsChangePassword: {
+		submit: 'Cambiar contraseña',
+		cancel: 'Cancelar'
+	},
+	_loginButtonsLoggedOutSingleLoginButton: {
+		signInWith: 'Iniciar sesión con',
+		configure: 'Configurar',
+		login: ''
+	},
+	_loginButtonsLoggedInSingleLogoutButton: {
+		signOut: 'Cerrar sesión'
+	},
+	_loginButtonsLoggedOut: {
+		noLoginServices: 'No hay ningún servicio configurado'
+	},
+	loginFields: {
+		'username-or-email': 'Usuario o contraseña',
+		'username': 'Usuario',
+		'email': 'Correo electrónico',
+		'password': 'Contraseña'
+	},
+	signupFields: {
+		'username': 'Usuario',
+		'email': 'Correo electrónico',
+		'email-opt': 'Correo elect. (opcional)',
+		'password': 'Contraseña',
+		'password-again': 'Contraseña (otra vez)'
+	}
+})
