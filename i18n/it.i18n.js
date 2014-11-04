0a1cf4693d36a1149212a76ba6144d163e760f14
Massimiliano Marini <massimiliano.marini@gmail.com>
2014-11-04 15:13:30 +0100

    Create it.i18n.js

---
+++ i18n/it.i18n.js
@@ -0,0 +1,85 @@
+i18n.map("it", {
+	resetPasswordDialog: {
+		title: "Reset your password",
+		newPassword: "Nuova password",
+		cancel: "Cancella",
+		submit: "Imposta password"
+	},
+	enrollAccountDialog: {
+		title: "Scegli una password",
+		newPassword: "Nuova password",
+		cancel: "Chiudi",
+		submit: "Imposta password"
+	},
+	justVerifiedEmailDialog: {
+		verified: "Indirizzo email verificato",
+		dismiss: "Dismiss"
+	},
+	loginButtonsMessagesDialog: {
+		dismiss: "Dismiss",
+	},
+	loginButtonsLoggedInDropdownActions: {
+		password: "Cambia password",
+		signOut: "Sign out"
+	},
+	loginButtonsLoggedOutDropdown: {
+		signIn: "Sign in",
+		up: "up"
+	},
+	loginButtonsLoggedOutPasswordServiceSeparator: {
+		or: "or"
+	},
+	loginButtonsLoggedOutPasswordService: {
+		create: "Create",
+		signIn: "Sign in",
+		forgot: "Password dimenticata?",
+		createAcc: "Crea nuovo account"
+	},
+	forgotPasswordForm: {
+		email: "Email",
+		reset: "Reset password",
+		sent: "Email inviata"
+	},
+	loginButtonsBackToLoginLink: {
+		back: "Cancella"
+	},
+	loginButtonsChangePassword: {
+		submit: "Cambia password",
+		cancel: "Cancella"
+	},
+	loginButtonsLoggedOutSingleLoginButton: {
+		signInWith: "Sign in with",
+		configure: "Configura",
+		login: "login"
+	},
+	loginButtonsLoggedInSingleLogoutButton: {
+		signOut: "Sign out"
+	},
+	loginButtonsLoggedOut: {
+		noLoginServices: "No login services configured"
+	},
+	loginFields: {
+		usernameOrEmail: "Username o Email",
+		username: "Username",
+		email: "Email",
+		password: "Password"
+	},
+	signupFields: {
+		username: "Username",
+		email: "Email",
+		emailOpt: "Email (opzionale)",
+		password: "Password",
+		passwordAgain: "Password (again)"
+	},
+	changePasswordFields: {
+		currentPassword: "Password corrente",
+		newPassword: "Nuova password",
+		newPasswordAgain: "Nuova password (again)"
+	},
+	errorMessages: {
+		usernameTooShort: "Username must be at least 3 characters long",
+		invalidEmail: "Invalid email",
+		passwordTooShort: "Password must be at least 6 characters long",
+		passwordsDontMatch: "Passwords don't match"
+	}
+});
