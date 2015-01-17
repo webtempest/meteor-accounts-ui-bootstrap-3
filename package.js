d89a6ec64371f752dd022d4d672ea22238a73833
Ian Martorell <ianmartorell@gmail.com>
2015-01-17 12:36:48 +0100

    Published #43 and #44 to atmosphere

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'ian:accounts-ui-bootstrap-3',
 	summary: 'Bootstrap-styled accounts-ui with multi-language support.',
-	version: '1.2.17',
+	version: '1.2.18',
 	git: "https://github.com/ianmartorell/meteor-accounts-ui-bootstrap-3"
 })
 
