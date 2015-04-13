05d31a76c9dcbec5dc25ac9fe75ab37871090a2e
Ian Martorell <ianmartorell@gmail.com>
2015-04-13 17:55:39 +0200

    Fixes #103

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'ian:accounts-ui-bootstrap-3',
 	summary: 'Bootstrap-styled accounts-ui with multi-language support.',
-	version: '1.2.45',
+	version: '1.2.46',
 	git: "https://github.com/ianmartorell/meteor-accounts-ui-bootstrap-3"
 })
 
