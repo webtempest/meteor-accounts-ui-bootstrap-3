83cfb90dc9fe371fd3bc04d28ce4db5ae6b7e1f6
Steven Scaffidi <sscaff1@gmail.com>
2015-06-09 21:50:58 -0500

    tired to fix click error

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'sscaff1:accounts-ui-bootstrap-3',
 	summary: 'Bootstrap-styled accounts-ui with multi-language support.',
-	version: '1.2.64',
+	version: '1.2.65',
 	git: "https://github.com/sscaff1/meteor-accounts-ui-bootstrap-3"
 })
 
