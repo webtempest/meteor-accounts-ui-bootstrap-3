c4d8b5651785dfc4dacf9fa05d54297fafc1ebba
Ian Martorell <ianmartorell@gmail.com>
2015-07-19 23:09:18 +0200

    Styling new modal

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'ian:accounts-ui-bootstrap-3',
 	summary: 'Bootstrap-styled accounts-ui with multi-language support.',
-	version: '1.2.75',
+	version: '1.2.76',
 	git: "https://github.com/ianmartorell/meteor-accounts-ui-bootstrap-3"
 })
 
